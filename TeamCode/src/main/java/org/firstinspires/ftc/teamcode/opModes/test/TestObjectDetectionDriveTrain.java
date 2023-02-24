8a5a61b00eec7b2a0a606ac09e3d09be43084494
sbottingota <saku.bottingota@gmail.com>
2023-02-24 16:45:47 +0000

    WIP

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/test/TestObjectDetectionDriveTrain.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.opModes.test;
+
+import com.arcrobotics.ftclib.hardware.SensorDistance;
+import com.arcrobotics.ftclib.hardware.SensorRevTOFDistance;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.libs.brightonCollege.modeBases.TeleOpModeBase;
+import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.HardwareMapContainer;
+
+@Disabled
+@TeleOp(name="Test Object Detection Drive Train [sbottingota]", group="Test")
+public class TestObjectDetectionDriveTrain extends TeleOpModeBase {
+    SensorDistance sensorDistance = new SensorRevTOFDistance(HardwareMapContainer.getMap(), )
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void every_tick() {
+
+    }
+
+    public
+}
