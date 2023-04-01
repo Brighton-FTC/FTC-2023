357a33f8c1e5041c20802469045a898cbe58cca4
12sliu <118719719+12sliu@users.noreply.github.com>
2023-04-02 00:05:34 +0100

    Work on H-drive trajectories
    Also changed some stuff in Mecanum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/test/TestHDriveTrajectory.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/test/TestHDriveTrajectory.java
@@ -1,12 +1,20 @@
 package org.firstinspires.ftc.teamcode.opModes.test;
 
+import com.arcrobotics.ftclib.command.RamseteCommand;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.arcrobotics.ftclib.controller.wpilibcontroller.RamseteController;
+import com.arcrobotics.ftclib.controller.wpilibcontroller.SimpleMotorFeedforward;
+import com.arcrobotics.ftclib.drivebase.DifferentialDrive;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.arcrobotics.ftclib.geometry.Translation2d;
 import com.arcrobotics.ftclib.hardware.RevIMU;
+import com.arcrobotics.ftclib.hardware.motors.MotorGroup;
 import com.arcrobotics.ftclib.kinematics.wpilibkinematics.ChassisSpeeds;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.DifferentialDriveKinematics;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.DifferentialDriveOdometry;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.DifferentialDriveWheelSpeeds;
 import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveKinematics;
 import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveOdometry;
 import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveWheelSpeeds;
@@ -16,6 +24,7 @@ import com.arcrobotics.ftclib.trajectory.TrajectoryGenerator;
 import com.arcrobotics.ftclib.util.Timing;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.modeBases.AutonomousModeBase;
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.HardwareMapContainer;
 
@@ -27,16 +36,18 @@ import java.util.ArrayList;
 
 @Autonomous(name="Mecanum Testing Trajectory", group="test")
 @Disabled
-public class TestMecanumTrajectory extends AutonomousModeBase { //TODO: Test
+public class TestHDriveTrajectory extends AutonomousModeBase { //TODO: Test
 
     private RevIMU imu;
     private Pose2d m_pose;
-    private MecanumDriveOdometry m_odometry;
+    private DifferentialDriveOdometry m_odometry;
     private Trajectory m_trajectory;
     private RamseteController controller;
     private Timing.Timer timer;
-    private MecanumDriveKinematics m_kinematics;
-    private MecanumDrive meca_drive;
+    private DifferentialDriveKinematics m_kinematics;
+    private DifferentialDrive m_drive;
+    private MotorGroup myMotors1;
+    private MotorGroup myMotors2;
 
     private double timeBeforeLastTick;
 
@@ -46,34 +57,35 @@ public class TestMecanumTrajectory extends AutonomousModeBase { //TODO: Test
         timer.start();
 
         // gyro
-        this.imu = new RevIMU(hardwareMap);
-        this.imu.init();
+        imu = new RevIMU(hardwareMap);
+        imu.init();
 
-        this.meca_drive = new MecanumDrive(HardwareMapContainer.motor0, HardwareMapContainer.motor1, HardwareMapContainer.motor2, HardwareMapContainer.motor3);
 
-        // Defining wheel locations relative to the center.
-        Translation2d m_frontLeftLocation =
-                new Translation2d(0.381, 0.381);
-        Translation2d m_frontRightLocation =
-                new Translation2d(0.381, -0.381);
-        Translation2d m_backLeftLocation =
-                new Translation2d(-0.381, 0.381);
-        Translation2d m_backRightLocation =
-                new Translation2d(-0.381, -0.381);
+        myMotors1 = new MotorGroup(HardwareMapContainer.motor0);
+        myMotors2 = new MotorGroup(HardwareMapContainer.motor1);
+
+        m_drive = new DifferentialDrive(myMotors1, myMotors2);
+
+//        // Defining wheel locations relative to the center.
+//        Translation2d m_frontLeftLocation =
+//                new Translation2d(0.381, 0.381);
+//        Translation2d m_frontRightLocation =
+//                new Translation2d(0.381, -0.381);
+//        Translation2d m_backLeftLocation =
+//                new Translation2d(-0.381, 0.381);
+//        Translation2d m_backRightLocation =
+//                new Translation2d(-0.381, -0.381);
 
         // Creating my kinematics object using the wheel locations.
-        m_kinematics = new MecanumDriveKinematics(
-            m_frontLeftLocation,
-            m_frontRightLocation,
-            m_backLeftLocation,
-            m_backRightLocation
+        m_kinematics = new DifferentialDriveKinematics(
+            0.5
         );
 
         // Creating my odometry object from the kinematics object. Here,
         // our starting pose is 5 meters along the long end of the field and in the
         // center of the field along the short end, facing forward.
-        m_odometry = new MecanumDriveOdometry(
-            m_kinematics,
+        // TODO: Look at game documentation to figure out what to write for this
+        m_odometry = new DifferentialDriveOdometry(
             this.imu.getRotation2d(),
             new Pose2d(5.0, 13.5, new Rotation2d())
         );
@@ -89,6 +101,8 @@ public class TestMecanumTrajectory extends AutonomousModeBase { //TODO: Test
         interiorWaypoints.add(new Translation2d(21.04, 18.23));
 
         m_trajectory = generateTrajectory(start, end, interiorWaypoints);
+
+        // Pretty sure we don't need to touch this, I hope
         controller = new RamseteController(2, 0.7);
     }
 
@@ -100,15 +114,17 @@ public class TestMecanumTrajectory extends AutonomousModeBase { //TODO: Test
         // Also update the Field2D object (so that we can visualize this in sim)
         Trajectory.State goal = m_trajectory.sample(timer.elapsedTime()); // sample the trajectory at 3.4 seconds from the beginning
         ChassisSpeeds adjustedSpeeds = controller.calculate(m_pose, goal);
-        MecanumDriveWheelSpeeds wheelSpeeds = m_kinematics.toWheelSpeeds(adjustedSpeeds);
-//        meca_drive.driveRobotCentric(adjustedSpeeds.vxMetersPerSecond, adjustedSpeeds.vyMetersPerSecond, adjustedSpeeds.omegaRadiansPerSecond,false);
+        DifferentialDriveWheelSpeeds wheelSpeeds = m_kinematics.toWheelSpeeds(adjustedSpeeds);
+
+
+//        m_drive.arcadeDrive(adjustedSpeeds.vxMetersPerSecond, adjustedSpeeds.vyMetersPerSecond, adjustedSpeeds.omegaRadiansPerSecond,false);
 
         // Get my gyro angle.
         Rotation2d gyroAngle = Rotation2d.fromDegrees(this.imu.getHeading());
 
         // Update the pose
         // Not sure if timer.elapsedTime() is how you're supposed to do it, test
-        m_pose = m_odometry.updateWithTime(timer.elapsedTime()-timeBeforeLastTick, gyroAngle, wheelSpeeds);
+        m_pose = m_odometry.update(gyroAngle, wheelSpeeds.leftMetersPerSecond, wheelSpeeds.rightMetersPerSecond);
         timeBeforeLastTick = timer.elapsedTime();
 
     }
@@ -121,7 +137,8 @@ public class TestMecanumTrajectory extends AutonomousModeBase { //TODO: Test
     }
 
     public Trajectory generateTrajectory(Pose2d sideStart, Pose2d crossScale,ArrayList interiorWaypoints) {
-        
+        // pretty sure we have to figure this out
+        // TODO: insert maxVelocityMetersPerSecond and maxAcclerationMetersPerSecondSq
         TrajectoryConfig config = new TrajectoryConfig(12, 12);
         config.setReversed(true);
 
